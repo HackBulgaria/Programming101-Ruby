09d4dbef83059b0b09477d109742cc1fa215d3f6
RadoRado <radorado@hackbulgaria.com>
2015-11-17 17:07:40 +0200

    Adding references and hello.rb for week1

---
+++ week01/README.md
@@ -0,0 +1,12 @@
+# Week 01 - Staring out with Ruby
+
+Ruby is a very beautiful & strange at first language.
+
+We are going to solve a lot of problems using it & making progress one step at a time. Don't expect the entire syntax from the first week :)
+
+## Getting to know Ruby
+
+* The first problem that you are going to face is to run the [hello.rb](hello.rb) file and examine the structures in it. This will give you clues for how to solve your problems.
+* [If you wonder what kind of loop you can use, take a loot at that reference](http://www.tutorialspoint.com/ruby/ruby_loops.htm)
+* [As you will see both double and single quotes, check this for the difference](http://stackoverflow.com/questions/6395288/double-vs-single-quotes)
+
---
+++ week01/hello.rb
@@ -0,0 +1,66 @@
+def hello_ruby
+  puts 'Hello Ruby'
+end
+
+def example_with_numbers
+  a = 5
+  b = 60
+
+  puts a + b
+
+  # This can be written also as:
+  # a +1 while a < b
+  while a < b do
+    a += 1
+  end
+
+  puts "a is #{a}, b is #{b}"
+end
+
+def example_reading_arguments
+  if ARGV.empty?
+    puts 'No arguments given'
+    return
+  end
+
+  arg = ARGV.first.to_i
+
+  if arg.even?
+    puts "Argument #{arg} is even"
+  else
+    puts "Argument #{arg} is odd"
+  end
+end
+
+# Here we have arrays / lists
+# The documentation about Arrays is here - http://ruby-doc.org/core-2.2.0/Array.html
+def sum(numbers)
+  result = 0
+  index = 0
+
+  while index < numbers.length
+    result += numbers[index]
+    index += 1
+  end
+
+  result
+end
+
+# Here we have hashes
+# The documentation about Hashes is here - http://ruby-doc.org/core-2.2.0/Hash.html
+def ruby_books
+  {
+    'Matz' => 'The Ruby Language',
+    'Black' => 'The Well-Grounded Rubyist'
+  }
+end
+
+# There is no need for () when calling functions / methods
+hello_ruby
+example_with_numbers
+example_reading_arguments
+puts sum([1, 2, 3, 4])
+
+books = ruby_books
+
+puts "Matz book is: #{books["Matz"]}" if books.key? 'Matz'
